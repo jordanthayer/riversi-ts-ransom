ed606167dc91970242ee5d09ccbc217247d8d2e7
Jordan Thayer <jordan.thayer@gmail.com>
2016-11-15 10:12:38 -0500

    Most of the important code for gameplay is now fleshed out

--- position.ts
+++ position.ts
@@ -1,8 +1,3 @@
-class BoardPos {
-    constructor(public row, public col){
-    }
-}
-
 interface Position {
     row : number;
     col : number;
@@ -26,9 +21,9 @@ var west = function (start : Position) {
 
 
 enum Player {
+    White = -1,
     None = 0,
-    White,
-    Black,
+    Black = 1,
 }
 
 var nextPlayer = function( current : Player ){
@@ -65,17 +60,65 @@ class Board {
     }
 
     public score(){
-        throw "stub implementation";
+        let result = 0
+        for(let x = 0; x < 8; x++){
+            for(let y = 0; y < 8; y++){
+                result += this.pieces[x][y];
+            }
+        }
+        return result;
     }
 
     private legalP(pos : Position, player : Player){
+
+        if(player != this.toPlay){
+            return MoveResponse.Illegal_Turn_Order;
+        }
+
+        if(pos.col < 0 || pos.col >= 8){
+            return MoveResponse.Illegal_Off_Board;
+        }
+
+        if(pos.row < 0 || pos.col >= 8){
+            return MoveResponse.Illegal_Off_Board;
+        }
+
+        let current = this.pieces[pos.col][pos.row];
+        if (current == Player.White || current == Player.Black){
+            return MoveResponse.Illegal_Occupied;
+        }
+
+        return MoveResponse.Legal;
     }
 
     public move(pos : Position, player : Player){
-        throw "stub implementation";
+        let legal = this.legalP(pos, player);
+        if(legal == MoveResponse.Legal){
+            this.pieces[pos.col][pos.row] = player;
+            this.toPlay = nextPlayer(player);
+        }
+        // here is where we would flip pieces in the various directions. [JTT 15-11-16]
+        throw "stub implementation"
+        //return legal;
     }
 
-    public prettyPrint(outChan){
-        throw "stub impelmentation";
+    public prettyPrint(){
+        for(let x = 0; x < 8; x++){
+            for(let y = 0; y < 8; y++){
+                let piece = this.pieces[x][y];
+                switch(piece){
+                case Player.None:
+                    console.log(' ');
+                    break;
+                case Player.White:
+                    console.log('X');
+                    break;
+                case Player.Black:
+                    console.log('O');
+                    break;
+                }
+            }
+            console.log('\n');
+        }
     }
 }
